e9aab75ac02297f85a211d9a0fde48a6bc9590d2
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-09-17 18:51:07 +0200

    Updated serde for variant with hasher generic

--- src/serde.rs
+++ src/serde.rs
@@ -1,4 +1,5 @@
 use crate::HyperLogLog;
+use core::hash::Hasher;
 
 #[derive(serde::Serialize, borsh::BorshSerialize)]
 enum HyperLogLogVariantRef<'a, const P: usize> {
@@ -14,30 +15,33 @@ enum HyperLogLogVariant<const P: usize> {
     Full(Vec<u8>),
 }
 
-impl<const P: usize> From<HyperLogLogVariant<P>> for HyperLogLog<P> {
+impl<H: Default + Hasher, const P: usize> From<HyperLogLogVariant<P>> for HyperLogLog<H, P> {
     fn from(value: HyperLogLogVariant<P>) -> Self {
         match value {
-            HyperLogLogVariant::Empty => HyperLogLog::<P>::new(),
+            HyperLogLogVariant::Empty => Self::new(),
             HyperLogLogVariant::Sparse { data } => {
                 let mut registers = vec![0; 1 << P];
                 for (index, val) in data {
                     registers[index as usize] = val;
                 }
 
-                HyperLogLog::<P> { registers }
+                Self::with_registers(registers)
             }
-            HyperLogLogVariant::Full(registers) => HyperLogLog::<P> { registers },
+            HyperLogLogVariant::Full(registers) => Self::with_registers(registers),
         }
     }
 }
 
-impl<'a, const P: usize> From<&'a HyperLogLog<P>> for HyperLogLogVariantRef<'a, P> {
-    fn from(hll: &'a HyperLogLog<P>) -> Self {
-        let none_empty_registers = HyperLogLog::<P>::number_registers() - hll.num_empty_registers();
+impl<'a, H: Default + Hasher, const P: usize> From<&'a HyperLogLog<H, P>>
+    for HyperLogLogVariantRef<'a, P>
+{
+    fn from(hll: &'a HyperLogLog<H, P>) -> Self {
+        let none_empty_registers =
+            HyperLogLog::<H, P>::number_registers() - hll.num_empty_registers();
 
         if none_empty_registers == 0 {
             HyperLogLogVariantRef::Empty
-        } else if none_empty_registers * 3 <= HyperLogLog::<P>::number_registers() {
+        } else if none_empty_registers * 3 <= HyperLogLog::<H, P>::number_registers() {
             // If the number of empty registers is larger enough, we can use sparse serialize to reduce the binary size
             // each register in sparse format will occupy 3 bytes, 2 for register index and 1 for register value.
             let sparse_data: Vec<(u16, u8)> = hll
@@ -60,7 +64,7 @@ impl<'a, const P: usize> From<&'a HyperLogLog<P>> for HyperLogLogVariantRef<'a, 
     }
 }
 
-impl<const P: usize> serde::Serialize for HyperLogLog<P> {
+impl<H: Default + Hasher, const P: usize> serde::Serialize for HyperLogLog<H, P> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
@@ -70,7 +74,7 @@ impl<const P: usize> serde::Serialize for HyperLogLog<P> {
     }
 }
 
-impl<'de, const P: usize> serde::Deserialize<'de> for HyperLogLog<P> {
+impl<'de, H: Default + Hasher, const P: usize> serde::Deserialize<'de> for HyperLogLog<H, P> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::Deserializer<'de>,
@@ -80,14 +84,14 @@ impl<'de, const P: usize> serde::Deserialize<'de> for HyperLogLog<P> {
     }
 }
 
-impl<const P: usize> borsh::BorshSerialize for HyperLogLog<P> {
+impl<H: Default + Hasher, const P: usize> borsh::BorshSerialize for HyperLogLog<H, P> {
     fn serialize<W: std::io::prelude::Write>(&self, writer: &mut W) -> std::io::Result<()> {
         let v: HyperLogLogVariantRef<'_, P> = self.into();
         v.serialize(writer)
     }
 }
 
-impl<const P: usize> borsh::BorshDeserialize for HyperLogLog<P> {
+impl<H: Default + Hasher, const P: usize> borsh::BorshDeserialize for HyperLogLog<H, P> {
     fn deserialize_reader<R: std::io::prelude::Read>(reader: &mut R) -> std::io::Result<Self> {
         let v = HyperLogLogVariant::<P>::deserialize_reader(reader)?;
         Ok(v.into())
@@ -101,7 +105,7 @@ mod tests {
     const P: usize = 14;
     #[test]
     fn test_serde() {
-        let mut hll = HyperLogLog::<P>::new();
+        let mut hll = HyperLogLog::<H, P>::new();
         json_serde_equal(&hll);
 
         for i in 0..100000 {
@@ -109,7 +113,7 @@ mod tests {
         }
         json_serde_equal(&hll);
 
-        let hll = HyperLogLog::<P>::with_registers(vec![1; 1 << P]);
+        let hll = HyperLogLog::<H, P>::with_registers(vec![1; 1 << P]);
         json_serde_equal(&hll);
     }
 
